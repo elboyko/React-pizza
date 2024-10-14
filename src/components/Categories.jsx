dd656ddc30be879e09cca9e8c32ec74f5345b4c1
elboyko <boyko.lena@gmail.com>
2024-10-14 19:51:47 +0700

    создала 3 компонента из верстки, использовала useState для рендеринга списка категорий пицц

---
+++ src/components/Categories.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    " Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((item, index) => {
+          return (
+            <li
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? "active" : ""}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+export default Categories;
